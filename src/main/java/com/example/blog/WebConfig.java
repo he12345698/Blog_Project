97493341e1db3cd50e554f8ed43dbbd562280f13
S.Huang <steven90411@gmail.com>
2024-08-16 11:01:33 +0800

    Added WebConfig.java: Configures CORS settings and general web configurations.

---
+++ src/main/java/com/example/blog/WebConfig.java
@@ -0,0 +1,30 @@
+package com.example.blog;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resourse/**")
+        .addResourceLocations("/resourse/");
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:3000") // 允許React前端的來源
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+
+}
