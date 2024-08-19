b6fbfd66426576ff03a1b649b6b8266fc85e3266
Y.Ko <kyc881201@gmail.com>
2024-08-19 21:58:43 +0800

    補足未import的組件

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../styles/pages/Register.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
