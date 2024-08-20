100d4f22ef3836ca5cc487076796c08f2678d446
S.Huang <steven90411@gmail.com>
2024-08-20 17:42:25 +0800

    新增文章實體

---
+++ src/main/java/com/example/blog/Vo/ArticleVo.java
@@ -0,0 +1,48 @@
+package com.example.blog.Vo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ArticleVo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String category;
+    private String content;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    @Override
+    public String toString() {
+        return "ArticleVo [id=" + id + ", title=" + title + ", category=" + category + ", content=" + content + "]";
+    }
+
+
+    
+    // Getters and setters
+}
