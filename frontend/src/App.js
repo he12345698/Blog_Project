aa15231347b4e7a8265a91bd0687dddd13a5264e
S.Huang <steven90411@gmail.com>
2024-08-20 17:35:30 +0800

    新增文章編輯器的路徑至Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Index from './index0';
 import LoginPage from './pages/LoginPage';
 import Register from './pages/Register';
 import Test1 from './Test1';
+import ArticleEditor from './pages/ArticleEditor';
 
 import './styles/App.css';
 
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/test1" element={<Test1 />} />
+        <Route path='/edit' element={<ArticleEditor />} />
         {/* 你可以在这里添加更多的路由 */}
       </Routes>
     </Router>
