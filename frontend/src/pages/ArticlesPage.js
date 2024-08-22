51ffddc52fad8f0c2adb123ce46ff8bbcc7640b2
Steven <steven90411@gmail.com>
2024-08-22 17:40:42 +0800

    加入讀取文章加載畫面

--- frontend/src/pages/ArticlesPage.js
+++ frontend/src/pages/ArticlesPage.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 import '../styles/pages/ArticlesPage.css';
 
 const ArticlesPage = () => {
-  const [articles, setArticles] = useState([]);
+  const [articleVo, setArticleVo] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
-    fetch(`http://localhost:8080/blog/api/articles?page=${currentPage}&size=10`)
+    fetch(`http://localhost:8080/blog-0.0.1-SNAPSHOT/api/articles?page=${currentPage}&size=10`)
       .then(response => response.json())
       .then(data => {
-        setArticles(data.content);
+        setArticleVo(data.content);
         setTotalPages(data.totalPages);
       });
   }, [currentPage]);
@@ -27,15 +27,21 @@ const ArticlesPage = () => {
     <main className="content">
       <section className="article-list">
         <div className="post-container">
-          {articles.map(article => (
-            <article className="post" key={article.id}>
-              <Link to={`/single-article/${article.id}`}>
-                {article.title} | 作者 : {article.author} | 更新於 : {new Date(article.updatedDate).toLocaleDateString()}
-              </Link>
-            </article>
-          ))}
+          {articleVo && articleVo.length > 0 ? (
+            articleVo.map(article => (
+              <article className="post" key={article.id}>
+                <Link to={`/edit-article/${article.id}`}>
+                  <div>
+                    {article.title} | 作者 : {article.author_id} | 更新於 : {new Date(article.last_edited_at).toLocaleDateString()}
+                  </div>
+                </Link>
+              </article>
+            ))
+          ) : (
+            <p>正在加載文章...</p> // 可以根據需求顯示加載狀態或提示語
+          )}
         </div>
-
+  
         {/* 分頁導航 */}
         <nav className="pagination">
           <Link to="#" onClick={() => handlePageChange(0)} className={currentPage === 0 ? 'active' : ''}>首頁</Link>
@@ -51,13 +57,13 @@ const ArticlesPage = () => {
           ))}
           <Link to="#" onClick={() => handlePageChange(totalPages - 1)} className={currentPage === totalPages - 1 ? 'active' : ''}>尾頁</Link>
         </nav>
-
       </section>
-
+  
       {/* 置頂按鈕 */}
       <button className="back-to-top">置頂</button>
     </main>
   );
+  
 };
 
 export default ArticlesPage;
