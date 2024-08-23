74b7ddd593e0d3d0e63c8b47b55a465baf1e988d
Y.Ko <kyc881201@gmail.com>
2024-08-23 10:50:09 +0800

    增加用戶文章區連接後端的邏輯

--- frontend/src/components/UserArticles.js
+++ frontend/src/components/UserArticles.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from "../styles/components/UserArticles.module.css";
 
 //用來顯示用戶發表的文章列表
 
-const UserArticles = ({ articles }) => {
+const UserArticles = () => {
+
+    // 管理文章資料
+    const [ articles, setAtrticles ] = useState([]);
+
+    // 獲取文章資料
+    useEffect(() => {
+        fetch('')
+        .then(response => response.json)
+        .then(data => {
+            setAtrticles(data);
+        })
+        .catch(error => {
+            console.log("獲取文章失敗", error);
+        })
+    });
+
     return (
         <div className="col-12">
             <h2>發表過的文章</h2>
