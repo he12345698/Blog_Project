15484af6e3f7420a3a4e9ca169dfabff2024c275
Steven <steven90411@gmail.com>
2024-08-24 11:42:47 +0800

    更改檔案路徑

--- src/main/java/com/example/blog/controller/HelloController.java
+++ src/main/java/com/example/blog/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.blog;
+package com.example.blog.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
