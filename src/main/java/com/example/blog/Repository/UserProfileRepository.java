824e9cbe66dd75a44041eeba4e27db7e74061980
Y.Ko <kyc881201@gmail.com>
2024-08-24 23:45:55 +0800

    更改Jpa裡的AccountVo數據類型

--- src/main/java/com/example/blog/Repository/UserProfileRepository.java
+++ src/main/java/com/example/blog/Repository/UserProfileRepository.java
@@ -13,7 +13,7 @@ import jakarta.transaction.Transactional;
 
 
 @Repository
-public interface UserProfileRepository extends JpaRepository<AccountVo, Long>{
+public interface UserProfileRepository extends JpaRepository<AccountVo, String>{
 	// 你可以在這裡定義一些自定義查詢方法，比如通過標題查詢文章
     //Spring Data JPA 使用方法名稱來推斷查詢邏輯。只要你新增的方法名稱不與預設方法重名，這些方法就能獨立運作，互不干涉。
     //自定義方法findBy:查詢 Title:屬性名稱 Containing:
